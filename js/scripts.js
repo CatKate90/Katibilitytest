301ed83c751f0142f9648eb650b14801ccb8c91a
Kathleen Auerbach <k.auerbach90@web.de>
2017-02-21 21:30:26 +0100

    all

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,9 @@ jQuery('.fs-title__content svg').click(function() {
   jQuery('.fs-title').css('z-index','1');
   jQuery('.cat').css('max-width','70px');
 });
+
+jQuery('input#q1a, input#q1c, input#q2a, input#q2c, input#q3b, input#q3c, input#q4a, input#q4c, input#q5b, input#q5c, input#q6a, input#q6b, input#q7a, input#q7c, input#q8a, input#q8c, input#q9a, input#q9b').click(function(e){
+  e.preventDefault();
+  jQuery('.fs-error').fadeIn(500);
+  jQuery('.fs-error').fadeOut(500);
+});
